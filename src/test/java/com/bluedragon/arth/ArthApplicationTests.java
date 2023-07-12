12dfabbc62ee03f2fb34c94e105a973634190547
최수원 <suwon3274@gmail.com>
2023-07-12 11:22:35 +0900

    init

---
+++ src/test/java/com/bluedragon/arth/ArthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluedragon.arth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
